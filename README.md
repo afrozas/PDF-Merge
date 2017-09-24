fceb852b97e5c952b2bd3bc46888ab2633767289
enigmaeth <enigmaeth@gmail.com>
2017-09-24 20:15:38 +0530

    Add file-cleanup, exception handling

--- README.md
+++ README.md
@@ -19,4 +19,7 @@ Now `cd` into the root of the directory and run the script `merge.py`.
 $ python merge.py
 ```
 
-The resultant PDF can be found inside the `Output` folder with the filename as printed on the terminal.
+- The script first converts all the non-PDF files to PDF format.
+- All files, including PDFs are moved from `ToMerge` to `Consumed`.
+- The converted PDFs are then merged into one and then deleted from inside the `ToMerge` folder.
+- Resultant PDF can be found inside the `Output` folder with the filename as printed on the terminal.
--- merge.py
+++ merge.py
@@ -1,15 +1,13 @@
-import os, subprocess, sys, shutil
+import os, subprocess, sys, time, shutil
 from PyPDF2 import PdfFileReader, PdfFileWriter, utils
 from cStringIO import StringIO
 import random
 
-path = "/home/enigmaeth/Code/PDFMerge/ToMerge"
+path = "ToMerge/"
 sys.stdout = sys.__stdout__
 
 def decompress_pdf(temp_buffer):
     temp_buffer.seek(0)  # Make sure we're at the start of the file.
- #    sys.stdout = os.fdopen(outfd, 'w')
-	# sys.stderr = os.fdopen(errfd, 'w')
     process = subprocess.Popen(['pdftk.exe',
                                 '-',  # Read from stdin.
                                 'output',
@@ -33,28 +31,50 @@ output = PdfFileWriter()
 list_files.sort()
 # Appending two pdf-pages from two different files
 for file in list_files:
-	file = "ToMerge/" + file
+	file = "ToMerge/" + str(file.replace(" ", "\ "))
 	if file[-3:] != 'pdf' and file[-10:] !='.gitignore':
-		command = "unoconv -f pdf " + str(file.replace(" ", "\ "))
+		command = "unoconv -f pdf " + str(file)
 		try:
-			command  += "; mv " + str(file.replace(" ", "\ ")) + " Consumed/"
+			print "  converting to PDF... :", file
+			command  += "; mv " + str(file) + " Consumed/"
 			subprocess.call(command, shell=True)
 		except:
 			print "Error while converting ", file, " to PDF"
-
-	if file[-3:] == 'pdf' :
+	elif file[-3:] == 'pdf':
+		try:
+			command = "cp " + str(file) + " Consumed/"
+			subprocess.call(command, shell=True)
+		except:
+			pass
+ 
+to_merge = os.listdir(path)
+list_files.sort()
+for file in to_merge:
+	time.sleep(1)
+	file = "ToMerge/" + str(file.replace("  ", "\ "))
+	if file[-3:] == 'pdf' and file[9] != '~':	
 		with open(file, 'rb') as input_file:
 			input_buffer = StringIO(input_file.read())
 		try:
-			print "   appending file...  :", file
+			print "   appending file...   :", file
 			append_pdf(PdfFileReader(input_buffer),output)
 		except utils.PdfReadError:
-			#append_pdf(PdfFileReader(decompress_pdf(input_buffer)),output)
-			print " [Error merging file] :", file , "skiping... " 
-			pass
+			try:
+				print "decompressing pdf  : ", file
+				append_pdf(PdfFileReader(decompress_pdf(input_buffer)),output)
+			except:
+				pass
+
 
 # Writing all the collected pages to a file
 hash = random.getrandbits(128)
 output_file = "Output/Combined" + str(hash)[:6] + str(".pdf")
 output.write(open(output_file,"wb"))
-print "   Generated PDF : ", output_file
+print "   Generated PDF  : ", output_file
+for the_file in os.listdir(path):
+    file_path = os.path.join(path, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception as e:
+        print(e)
